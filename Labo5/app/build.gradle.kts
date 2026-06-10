dda61dbb6fc18250ab6d6b7dbcd64147adccffd7
AlejandroAlv12 <00211623@uca.edu.sv>
2026-06-09 19:10:26 -0600

    feat: Agregar Room dependencies, KSP plugin y definir Task entity

--- Labo5/app/build.gradle.kts
+++ Labo5/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinCompose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -52,6 +53,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Labo5/app/src/main/java/com/pdm0126/labo5/model/Task.kt
+++ Labo5/app/src/main/java/com/pdm0126/labo5/model/Task.kt
@@ -1,9 +1,13 @@
 package com.pdm0126.labo5.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 
+@Entity(tableName = "tasks")
 data class Task (
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val title: String,
     val description: String,
     val endDate: Date = Date(),
