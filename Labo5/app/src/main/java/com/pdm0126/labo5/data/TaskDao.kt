cccd4d72d409695eadb8742b0c43819241789746
AlejandroAlv12 <00211623@uca.edu.sv>
2026-06-09 19:12:11 -0600

    feat: Implementar TaskDao, Converters y AppDatabase

---
+++ Labo5/app/src/main/java/com/pdm0126/labo5/data/TaskDao.kt
@@ -0,0 +1,20 @@
+package com.pdm0126.labo5.data
+
+import androidx.room.*
+import com.pdm0126.labo5.model.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks ORDER BY id DESC")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+}
